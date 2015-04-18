595a896c4ce9397ca93bf44044821e0ffa51375c
matthewnrauch <matthew.rauch@hotmail.com>
2015-04-18 16:36:08 -0600

    Update db.js
    Added links to information about IndexedDB and an example link of an app using IndexedDB that we can use for information.

--- db.js
+++ db.js
@@ -1,23 +1,22 @@
 //Database JavaScript file
 //https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+//-->https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Basic_Concepts_Behind_IndexedDB
+//-->https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+//--Example>https://mdn.mozillademos.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB$samples/Full_IndexedDB_example?revision=783011
 //http://mdn.github.io/to-do-notifications/scripts/todo.js
 //https://google-developers.appspot.com/chart/interactive/docs/php_example
 //http://stackoverflow.com/questions/12607251/how-do-i-store-json-objects-in-indexeddb
 
 //Structure:
-//
-//
+//Include the database structure, what elements do you want to store?
+//course_id, course_section_id, period, start_time, end_time, title, teacher_name, room_#
 
 //Elements:
 //I will create the link to Kuna Schools Data Warehouse
 //You can call the data using this form:
-//			var yearColumnData = $.ajax({
-//				url: '../include/student.inc.php?s=studentid', //replace studentid with the actual id
-//				dataType: 'json',
-//				async: false
-//			}).responseText;
+//var dbData = $.ajax({
+//	url: '../include/student.inc.php?s=studentid', //replace studentid with the actual id
+//	dataType: 'json',
+//	async: false
+//}).responseText;
 
-var db;
-var newItem = [
-	{student_id: "", section_id: "", teacher_name: ""}
-];
